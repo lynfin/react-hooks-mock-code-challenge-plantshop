084117e9041b1599d04e704e391ffc5e0fbb0521
Lyn <lynfinman@gmail.com>
2022-06-15 14:20:20 -0400

    added useX

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
