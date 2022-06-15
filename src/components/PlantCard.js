79373d782ddbbd0ee0abc43e80a2482908ba3f9c
Lyn <lynfinman@gmail.com>
2022-06-15 15:04:37 -0400

    added isInStock state to plantCard

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 function PlantCard({ plant }) {
   const { name, image, price } = plant;
+  const [isInStock, setIsInStock] = useState(true);
+
   return (
     <li className="card">
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>Price: {price}</p>
-      {true ? (
+      {isInStock ? (
         <button className="primary">In Stock</button>
       ) : (
         <button>Out of Stock</button>
